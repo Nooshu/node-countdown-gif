3b33a0ab22247f5ce229d0692447c689f5965d52
Matt Hobbs <matthew.hobbs@digitaslbi.com>
2016-06-22 23:06:48 +0100

    Updated the port setting in the app due to crashing on Heroku.

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ app.get('/serve', function (req, res) {
     });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 module.exports = app;
